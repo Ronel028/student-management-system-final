3d4f91504c8350f5c2426499167970116945c328
Ronel028 <shujihanma028@gmail.com>
2022-12-11 19:59:47 -0800

    initial commit

---
+++ dashboard/dashboard.php
@@ -0,0 +1,240 @@
+
+<?php include_once('../partials/head.php') ?>
+    
+    <div class="container-scroller">
+
+        <!-- navigation -->
+        <?php include_once("../partials/navigation.php") ?>
+
+        <div class="container-fluid page-body-wrapper">
+            <!-- sidebar -->
+            <?php include_once("../partials/sidebar.php") ?>
+
+            <div class="main-panel">
+                <div class="content-wrapper">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="home-tab">
+
+                                <div class="statistics-details row">
+                                    <div class="col">
+                                        <div class="card bg-primary card-rounded">
+                                            <div class="card-body">
+                                                <h4 class="card-title card-title-dash text-white mb-4">Student</h4>
+                                                <div>
+                                                    <div>
+                                                        <h2 class="text-info">100</h2>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <div class="card bg-primary card-rounded">
+                                            <div class="card-body">
+                                                <h4 class="card-title card-title-dash text-white mb-4">Staff</h4>
+                                                <div>
+                                                    <div>
+                                                        <h2 class="text-info">50</h2>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <div class="card bg-primary card-rounded">
+                                            <div class="card-body">
+                                                <h4 class="card-title card-title-dash text-white mb-4">Teacher</h4>
+                                                <div>
+                                                    <div>
+                                                        <h2 class="text-info">200</h2>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <div class="card bg-primary card-rounded">
+                                            <div class="card-body">
+                                                <h4 class="card-title card-title-dash text-white mb-4">Course</h4>
+                                                <div>
+                                                    <div>
+                                                        <h2 class="text-info">12</h2>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="card">
+                                        <div class="card-body">
+                                        <h4 class="card-title">Bordered table</h4>
+                                        <p class="card-description">
+                                            Add class <code>.table-bordered</code>
+                                        </p>
+                                        <div class="table-responsive pt-3">
+                                            <table class="table table-bordered">
+                                            <thead>
+                                                <tr>
+                                                    <th>#</th>
+                                                    <th>First name</th>
+                                                    <th>Progress</th>
+                                                    <th>Amount</th>
+                                                    <th>Deadline</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                <td>1</td>
+                                                <td>Herman Beck</td>
+                                                <td>
+                                                    <div class="progress">
+                                                        <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                                                    </div>
+                                                </td>
+                                                <td>$ 77.99</td>
+                                                <td>
+                                                    May 15, 2015
+                                                </td>
+                                                </tr>
+                                                <tr>
+                                                <td>
+                                                    2
+                                                </td>
+                                                <td>
+                                                    Messsy Adam
+                                                </td>
+                                                <td>
+                                                    <div class="progress">
+                                                    <div class="progress-bar bg-danger" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                                                    </div>
+                                                </td>
+                                                <td>
+                                                    $245.30
+                                                </td>
+                                                <td>
+                                                    July 1, 2015
+                                                </td>
+                                                </tr>
+                                                <tr>
+                                                <td>
+                                                    3
+                                                </td>
+                                                <td>
+                                                    John Richards
+                                                </td>
+                                                <td>
+                                                    <div class="progress">
+                                                    <div class="progress-bar bg-warning" role="progressbar" style="width: 90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                                                    </div>
+                                                </td>
+                                                <td>
+                                                    $138.00
+                                                </td>
+                                                <td>
+                                                    Apr 12, 2015
+                                                </td>
+                                                </tr>
+                                                <tr>
+                                                <td>
+                                                    4
+                                                </td>
+                                                <td>
+                                                    Peter Meggik
+                                                </td>
+                                                <td>
+                                                    <div class="progress">
+                                                    <div class="progress-bar bg-primary" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                                    </div>
+                                                </td>
+                                                <td>
+                                                    $ 77.99
+                                                </td>
+                                                <td>
+                                                    May 15, 2015
+                                                </td>
+                                                </tr>
+                                                <tr>
+                                                <td>
+                                                    5
+                                                </td>
+                                                <td>
+                                                    Edward
+                                                </td>
+                                                <td>
+                                                    <div class="progress">
+                                                    <div class="progress-bar bg-danger" role="progressbar" style="width: 35%" aria-valuenow="35" aria-valuemin="0" aria-valuemax="100"></div>
+                                                    </div>
+                                                </td>
+                                                <td>
+                                                    $ 160.25
+                                                </td>
+                                                <td>
+                                                    May 03, 2015
+                                                </td>
+                                                </tr>
+                                                <tr>
+                                                <td>
+                                                    6
+                                                </td>
+                                                <td>
+                                                    John Doe
+                                                </td>
+                                                <td>
+                                                    <div class="progress">
+                                                    <div class="progress-bar bg-info" role="progressbar" style="width: 65%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+                                                    </div>
+                                                </td>
+                                                <td>
+                                                    $ 123.21
+                                                </td>
+                                                <td>
+                                                    April 05, 2015
+                                                </td>
+                                                </tr>
+                                                <tr>
+                                                <td>
+                                                    7
+                                                </td>
+                                                <td>
+                                                    Henry Tom
+                                                </td>
+                                                <td>
+                                                    <div class="progress">
+                                                    <div class="progress-bar bg-warning" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
+                                                    </div>
+                                                </td>
+                                                <td>
+                                                    $ 150.00
+                                                </td>
+                                                <td>
+                                                    June 16, 2015
+                                                </td>
+                                                </tr>
+                                            </tbody>
+                                            </table>
+                                        </div>
+                                        </div>
+                                    </div>
+
+                            </div>
+                        <div>
+                    </div>
+                </div>
+
+
+
+                <!-- partial:partials/_footer.html -->
+                <footer class="footer">
+                    <div class="d-sm-flex justify-content-center justify-content-sm-between">
+                        <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Premium <a href="https://www.bootstrapdash.com/" target="_blank">Bootstrap admin template</a> from BootstrapDash.</span>
+                        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Copyright © 2021. All rights reserved.</span>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        
+    </div>
+
+<?php include_once('../partials/footer.php') ?>
